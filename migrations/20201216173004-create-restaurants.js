5d10482c3eb426ff717d577b7b5c612365f8da20
fathan-nurudin <fathan011197@gmail.com>
2020-12-17 00:44:23 +0700

    add-tables-users-restaurants-Bookings

---
+++ migrations/20201216173004-create-restaurants.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Restaurants', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      opening_hours: {
+        type: Sequelize.INTEGER
+      },
+      closing_hours: {
+        type: Sequelize.INTEGER
+      },
+      StatusId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Restaurants');
+  }
+};
