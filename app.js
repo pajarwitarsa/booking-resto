534cfab3b09a603eed48c6ca4158099a655e124a
pajarwitarsa <pajarwitarsa@gmail.com>
2020-12-18 01:50:07 +0700

    Deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = require('./routes/index.js');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 const session = require('express-session')
 
 app.use(session({
